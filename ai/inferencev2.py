7500445c7c1d578fdd8c42bb7c512b1ebd052c2e
euzhengxi <euzhengxi110@gmail.com>
2025-11-15 15:21:08 +0800

    latest ai codes

--- ai/inferencev2.py
+++ ai/inferencev2.py
@@ -15,17 +15,19 @@ lateral raises:
 
 BATCH_SIZE = 1
 device = torch.device("cpu")
+POSE_MODEL = "models/yolo11n-pose.pt"
+pose_model = YOLO(POSE_MODEL)
 NN_model = NN().to(device)
 NN_model.load_state_dict(torch.load(f'models/model_epoch_74.pt', weights_only=True)) 
 NN_model.eval()
 
 def yolo_inference(imageList: list):
-    inference_dataloader = preprocessing(imageList=imageList, batchSize=BATCH_SIZE)
+    poses = preprocessing(pose_model=pose_model, imageList=imageList)
     result = []
     
     with torch.inference_mode():
-        for pose in inference_dataloader:
-            pose = pose.to(torch.float32).to(device)
+        for pose in poses:
+            pose = torch.tensor(pose).to(torch.float32).to(device)
             logits = NN_model(pose)
             probabilities = torch.sigmoid(logits).item()
             print(probabilities)
@@ -33,5 +35,5 @@ def yolo_inference(imageList: list):
 
 if __name__ == "__main__":
     start = time.time()
-    yolo_inference(["e1_1.png", "e3_2.png", "e3_1.png"])
+    yolo_inference(["t1_1.jpg"])
     print(f'Inference time: {((time.time() - start) * 1000):.4f} ms')
